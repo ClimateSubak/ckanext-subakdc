40ca3b0d07746f147095d05bece3eb06309f3249
Laurence Watson <laurence.watson@gmail.com>
2022-01-10 08:46:47 +0000

    subak colours into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./ckanext/subakdc/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkblue: '#0f2b43',
+        orange: '#c95e3b',
+        grey: '#eeeeec',
+        white: '#ffffff',
+        teal: '#1d838b',
+        pink: '#b0566b',
+        blue: '#1b658f',
+        lightblue: '#3bc8ec'
+      },
+    },
   },
   plugins: [],
 }
