a066819fc1ae33fdf863556f1e9618247a1032b1
Christopher Briggs <confusedmatrix@gmail.com>
2022-01-20 15:27:06 +0000

    Moved cookie banner to webassets

--- ckanext/subakdc/plugin.py
+++ ckanext/subakdc/plugin.py
@@ -5,8 +5,8 @@ import ckan.plugins.toolkit as toolkit
 class SubakdcPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
 
-    # IConfigurer
+    # ------- IConfigurer method implementations ------- #
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
-        toolkit.add_resource('./assets', 'subakdc_assets')
+        toolkit.add_resource('./assets', 'ckanext-subakdc')
